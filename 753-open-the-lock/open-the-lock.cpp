3e0e30c2ee918f790ce934da4e34a0eb897133a0
rokhiya-hub <rokhiyab5@gmail.com>
2026-07-25 11:29:58 +0530

    Time: 512 ms (5.65%) | Memory: 40.9 MB (50.85%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+char clockwise(char c)
+{
+    if(c=='9')
+    return '0';
+
+    return c+1;
+}
+char anticlockwise(char c)
+{
+    if(c=='0')
+    return '9';
+
+    return c-1;
+} 
+
+    int openLock(vector<string>& deadends, string target) {
+
+        set<string>dead(deadends.begin(),deadends.end());
+
+        if(dead.count("0000"))
+        return -1;
+
+        set<string>vis;
+        queue<string>q;
+        q.push("0000");
+        vis.insert("0000");
+
+        int steps=0;
+
+        while(!q.empty()){
+
+            int n = q.size();
+
+            while(n--)
+            {
+                string curr = q.front();
+                q.pop();
+
+                if(curr==target)
+                return steps;
+
+                for(int i=0;i<4;i++)
+                {
+                    string next = curr;
+                    next[i]=clockwise(curr[i]);
+
+                    // if(next==target)
+                    // return steps;
+
+                    if(!dead.count(next) && !vis.count(next) )
+                    {
+                        q.push(next);
+                        vis.insert(next);
+                    }
+
+                    next = curr;
+                    next[i]=anticlockwise(curr[i]);
+                    // if(next==target)
+                    //  return steps;
+                     
+                     if(!dead.count(next) && !vis.count(next))
+                     {
+                        q.push(next);
+                        vis.insert(next);
+                     }
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
