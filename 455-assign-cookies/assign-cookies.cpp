13922fde38346281a12723edc97c91bc79c3fe71
rokhiya-hub <rokhiyab5@gmail.com>
2026-09-05 09:36:44 +0530

    Time: 5 ms (80.14%) | Memory: 44.7 MB (97.34%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0;
+        for(int j=0;j<s.size();j++){
+            if(i<g.size()){
+                if(s[j]>=g[i])
+                i++;       
+            }      
+        }
+        return i;
+    }
+};
