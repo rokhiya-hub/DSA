cb31efa9036d072211b536650d044e263bc90002
rokhiya-hub <rokhiyab5@gmail.com>
2026-09-08 21:57:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int total = 0;
+        int threshold = 1000;
+
+        while (threshold <= n){
+            total+=n-threshold+1;
+            threshold*=1000;
+        }
+        return total;
+    }
+};
